25496523691a2a1bf66f9237025becfd55a644b2
sajeeb <sajeebashraf@gmail.com>
2022-03-14 02:03:07 +0600

    Add project files.

---
+++ bKashASPCore/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bKashASPCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            bKashCore.bKashApi.callBack = paymentDone;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public void paymentDone(object data)
+        {
+
+        }
+    }
+}
